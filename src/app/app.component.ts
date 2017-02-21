1c68b8b2b9145c785210287792bf4f2a62017a67
ekhamis <ekhamis@gmail.com>
2017-02-21 01:12:47 +0100

    enhancements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular W1 HomeWork';
+  title = 'Angular W1 HomeWork'
   fullName = 'Eyad Khamis'
 }
--- src/app/todos.component.ts
+++ src/app/todos.component.ts
@@ -4,10 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-todos',
   templateUrl: './todos.component.html',
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent {
   tasks = []
 
-  addTask(newTask: string) {
+  addTask(newTask) {
     if (newTask) {
       this.tasks.push(newTask)
       let index = this.tasks.indexOf(newTask)
@@ -15,16 +15,10 @@ export class TodosComponent implements OnInit {
     }
   }
 
-  removeTask(task: string) {
+  removeTask(task) {
     if (task) {
       let index = this.tasks.indexOf(task)
       this.tasks.splice(index, 1)
     }
   }
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
